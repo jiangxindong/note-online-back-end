40fd8373201558583b7a242a65ae506c1f0d2b97
jiangxindong <jiangxindong98@qq.com>
2022-05-09 16:00:50 +0800

    feat: login api

--- app/service/auth.js
+++ app/service/auth.js
@@ -1,20 +1,50 @@
 const { Service } = require('egg');
+const { CODE } = require('@/public/constant/error');
 class AuthService extends Service {
   async login(params) {
-    const { ctx } = this;
-    ctx.body = await ctx.model.User.findAll(params);
+    const { ctx, app } = this;
+    const { username, password } = params;
+    const user = await ctx.model.User.findAll({
+      where: {
+        username,
+      },
+    });
+    if (!user.length) {
+      ctx.throwError(CODE.NOT_EXISTED_USERNAME);
+    }
+    if (user[0].password !== password) {
+      ctx.throwError(CODE.WRONG_PASSWORD);
+    }
+    if (user[0].password === password) {
+      const token = app.jwt.sign(
+        {
+          username,
+        },
+        app.config.verifyToken.secret
+      );
+      return ctx.formatResp(token);
+    }
+    ctx.throwError(CODE.UNKNOWN_ERROR);
   }
   async create() {
     const ctx = this.ctx;
     const { username, password, nickName, phoneNumber } = ctx.request.body;
-    const user = await ctx.model.User.create({
-      username,
-      password,
-      nickName,
-      phoneNumber,
+    const user = await ctx.model.User.findAll({
+      where: {
+        username,
+      },
     });
-    ctx.status = 201;
-    ctx.body = user;
+    if (!user.length) {
+      return ctx.formatResp(
+        await ctx.model.User.create({
+          username,
+          password,
+          nickName,
+          phoneNumber,
+        })
+      );
+    }
+    ctx.throwError(CODE.EXISTED_USERNAME);
   }
 }
 module.exports = AuthService;
